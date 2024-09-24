63bdfe74639d4ec0816f89152c530ce03ce70a51
Sreeni <seenu619@gmail.com>
2024-09-24 14:55:17 +0530

    balance issue fix

--- application/views/student/fees.php
+++ application/views/student/fees.php
@@ -166,28 +166,32 @@
                         ?>
                         <div class="table-responsive">
                             <table class="table table-striped mb-0">
-                                <thead class="thead-light text-center">
-                                    <tr>
-                                        <th rowspan="2">ACADEMIC YEAR</th>
-                                        <th rowspan="2">YEAR</th>
-                                        <th colspan="3">CORPUS FEE (&#8377;)</th>
-                                        <th colspan="3">COLLEGE FEE (&#8377;)</th>
+                                <thead class="thead-light text-center align-middle">
+                                <tr class="text-center">
+                                        <th rowspan="2" width="11%" class="align-middle">ACADEMIC YEAR</th>
+                                        <th rowspan="2" width="11%" class="align-middle">YEAR</th>
+                                        <th colspan="3" class="">CORPUS FEE (&#8377;)</th>
+                                        <th colspan="3" class="">COLLEGE FEE (&#8377;)</th>
                                     </tr>
                                     <tr class="text-center">
-                                        <th>DEMAND</th>
-                                        <th>COLLECTION</th>
-                                        <th>BALANCE</th>
-                                        <th>DEMAND</th>
-                                        <th>COLLECTION</th>
-                                        <th>BALANCE</th>
+                                        <th width="13%">DEMAND</th>
+                                        <th width="13%">COLLECTION</th>
+                                        <th width="13%">BALANCE</th>
+                                        <th width="13%">DEMAND</th>
+                                        <th width="13%">COLLECTION</th>
+                                        <th width="13%">BALANCE</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <?php
                                     foreach ($fees as $fee) {
 
+                                        // print_r($fee);
                                         $corpus_fee_demand = $fee->corpus_fee_demand;
-                                        $corpus_fee_balance = $corpus_fee_demand - $this->admin_model->get_total_amount($fee->year, $student->usn, 1);
+                                        $corpus_fee_collection = $fee->corpus_fee_collection;
+                                        $collection_amount = $this->admin_model->get_total_amount($fee->year, $student->usn, 1);
+                                        $paid_amount = $corpus_fee_collection + $collection_amount;
+                                        $corpus_fee_balance = number_format($corpus_fee_demand - $paid_amount,2);
 
                                         if ($corpus_fee_balance > 0) {
                                             $corpus_pay_btn = '<form action="' . base_url(htmlspecialchars($action)) . '" method="post" class="user">
@@ -197,17 +201,21 @@
                                             <input type="hidden" name="aided_unaided" id="aided_unaided" value="' . htmlspecialchars($student->sub_quota) . '">
                                             <input type="hidden" name="mobile" id="mobile" value="' . htmlspecialchars($student->student_number) . '">
                                             <input type="hidden" name="amount" id="amount" value="' . htmlspecialchars($corpus_fee_balance) . '">
-                                             <input type="hidden" name="year" id="year" value="' . htmlspecialchars($fee->year) . '">
-                                           <input type="hidden" name="payment_mode" id="payment_mode" value="1">
+                                            <input type="hidden" name="year" id="year" value="' . htmlspecialchars($fee->year) . '">
+                                            <input type="hidden" name="payment_mode" id="payment_mode" value="1">
                                             <button type="submit" class="btn btn-danger btn-sm" name="Update" id="Update">PAY FEE</button>
-                                        </form>';
+                                            </form>';
                                         } else {
                                             $corpus_pay_btn = '';
                                         }
+
                                         $college_fee_demand = $fee->college_fee_demand;
                                         $college_fee_collection = $fee->college_fee_collection;
-                                        $college_fee = $fee->college_fee_demand - $fee->college_fee_collection;
-                                        $college_fee_balance = $college_fee - $this->admin_model->get_total_amount($fee->year, $student->usn, 0);
+                                        $college_collection_amount = $this->admin_model->get_total_amount($fee->year, $student->usn, 0);
+                                        // $college_fee = $fee->college_fee_demand - $fee->college_fee_collection;
+                                        $college_paid_fee = $college_fee_collection + $college_collection_amount;
+                                        // $college_fee_balance = $college_fee - $this->admin_model->get_total_amount($fee->year, $student->usn, 0);
+                                        $college_fee_balance = number_format($college_fee_demand - $college_paid_fee,2);
 
                                         if ($college_fee_balance > 0) {
                                             $college_pay_btn = '<form action="' . base_url(htmlspecialchars($action)) . '" method="post" class="user">
@@ -216,11 +224,11 @@
                                             <input type="hidden" name="email" id="email" value="' . htmlspecialchars($student->email) . '">
                                             <input type="hidden" name="aided_unaided" id="aided_unaided" value="' . htmlspecialchars($student->sub_quota) . '">
                                             <input type="hidden" name="mobile" id="mobile" value="' . htmlspecialchars($student->student_number) . '">
-                                            <input type="hidden" name="amount" id="amount" value="' . htmlspecialchars($college_fee) . '">
-                                             <input type="hidden" name="year" id="year" value="' . htmlspecialchars($fee->year) . '">
-                                           <input type="hidden" name="payment_mode" id="payment_mode" value="0">
+                                            <input type="hidden" name="amount" id="amount" value="' . htmlspecialchars($college_fee_balance) . '">
+                                            <input type="hidden" name="year" id="year" value="' . htmlspecialchars($fee->year) . '">
+                                            <input type="hidden" name="payment_mode" id="payment_mode" value="0">
                                             <button type="submit" class="btn btn-danger btn-sm" name="Update" id="Update">PAY FEE</button>
-                                        </form>';
+                                            </form>';
                                         } else {
                                             $college_pay_btn = '';
                                         }
@@ -228,8 +236,8 @@
                                         echo "<tr>";
                                         echo "<td class='text-center'>" . $fee->academic_year . "</td>";
                                         echo "<td class='text-center'>" . $fee->year . "</td>";
-                                        echo "<td class='text-center'>" . $fee->corpus_fee_demand . "</td>";
-                                        echo "<td class='text-center'>" . $fee->corpus_fee_collection . "</td>";
+                                        echo "<td class='text-center'>" . indian_number_format($fee->corpus_fee_demand) . "</td>";
+                                        echo "<td class='text-center'>" . indian_number_format($fee->corpus_fee_collection) . "</td>";
                                         echo "<td class='text-center'>" . indian_number_format($corpus_fee_balance) . '  ' . $corpus_pay_btn . "</td>";
                                         echo "<td class='text-center'>" . indian_number_format($college_fee_demand) . "</td>";
                                         echo "<td class='text-center'>" . indian_number_format($college_fee_collection) . "</td>";
@@ -276,7 +284,7 @@
                                 // if ($transactionDetails1->transaction_type == 3) {
                                 //     $trans = $transactionTypes[$transactionDetails1->transaction_type] . "<br> No:" . $transactionDetails1->reference_no . '<br> Dt:' . date('d-m-Y', strtotime($transactionDetails1->reference_date));
                                 // }
-
+                        
                                 if ($transactionDetails1->transaction_type == 1) {
                                     $trans = $voucher_types[$transactionDetails1->transaction_type] . "<br> No:" . $transactionDetails1->reference_no . '<br> Dt:' . date('d-m-Y', strtotime($transactionDetails1->transaction_date));
                                     $receiptprint = $transactionDetails1->receipt_no;
@@ -304,14 +312,14 @@
                                 // }else if($transactionDetails1->transaction_status == 2){
                                 //     $transaction_status = "<span class='text-danger'>Cancelled</span><br><span class='text-dark'>".nl2br($transactionDetails1->remarks)."</span>";
                                 // }else{
-
+                        
                                 //     $transaction_status = "<span class='text-warning'>Processing</span> <br>".anchor('admin/approvePayment/'.$transactionDetails1->id,'Approve','class="btn btn-info btn-sm"').' '.anchor('admin/deletePayment/'.$transactionDetails1->id.'/'.$transactionDetails1->admissions_id,'Delete','class="btn btn-danger btn-sm"');
                                 // }
-
+                        
                                 $result_array = array(
                                     $i++,
                                     ($transactionDetails1->receipt_no) ? anchor('student/downloadReceipt/' . $transactionDetails1->reg_no . '/' . $transactionDetails1->id, $transactionDetails1->receipt_no) : "-",
-                                    // $transactionDetails1->receipt_no,
+                                        // $transactionDetails1->receipt_no,
                                     ($transactionDetails1->transaction_date != "") ? date('d-m-Y', strtotime($transactionDetails1->transaction_date)) : "-",
                                     $trans,
                                     number_format($transactionDetails1->amount, 2),
